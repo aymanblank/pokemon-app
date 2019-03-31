f9645ec5612598a7a1dddc7a51658fba0603de19
Ayman Abu Khalifa <Aymanblank@gmail.com>
2019-03-31 21:39:37 +0300

    Added redux provider with it's store

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 import apolloClient from './apollo';
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={apolloClient}>
-        <Router>
-          <NavBar />
-          <Route exact path="/" component={null} />
-          <Route exact path="/pokemon/:pokemon_id" component={null} />
-        </Router>
-      </ApolloProvider>
+      <Provider store={store}>
+        <ApolloProvider client={apolloClient}>
+          <Router>
+            <NavBar />
+            <Route exact path="/" component={null} />
+            <Route exact path="/pokemon/:pokemon_id" component={null} />
+          </Router>
+        </ApolloProvider>
+      </Provider>
     );
   }
 }
