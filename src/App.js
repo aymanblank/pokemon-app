22387db13eaf4baf24053de0eed1ab5eddf15939
Ayman Abu Khalifa <Aymanblank@gmail.com>
2019-03-31 20:31:55 +0300

    Adding react-router-dom | prop-types | check-prop-types | styled-components & adding Routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Router>
         <NavBar />
-      </div>
+        <Route exact path="/" component={null} />
+        <Route exact path="/pokemon/:pokemon_id" component={null} />
+      </Router>
     );
   }
 }
