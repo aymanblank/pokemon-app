027d29154d6c08db15e77c396c05a8f58dbcd7ad
Ayman Abu Khalifa <Aymanblank@gmail.com>
2019-03-31 20:44:33 +0300

    Adding Apollo Provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import apolloClient from './apollo';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <NavBar />
-        <Route exact path="/" component={null} />
-        <Route exact path="/pokemon/:pokemon_id" component={null} />
-      </Router>
+      <ApolloProvider client={apolloClient}>
+        <Router>
+          <NavBar />
+          <Route exact path="/" component={null} />
+          <Route exact path="/pokemon/:pokemon_id" component={null} />
+        </Router>
+      </ApolloProvider>
     );
   }
 }
